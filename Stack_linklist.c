68eaef18202429912caaaa81624dcd1bd4d5fa57
taparia11 <taparianikhil13@gmail.com>
2021-06-05 09:09:32 +0530

    stack_linklist

---
+++ Stack_linklist.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node* next;
+};
+
+struct node* top;
+
+int isEmpty(){
+    if(top==NULL)
+        return 1;
+
+    return 0;
+}
+
+
+int isFull(){
+
+    struct node* ptr = (struct node*)malloc(sizeof(struct node));
+    if (ptr==NULL)
+    {
+        return 1;
+    }
+    
+    return 0;
+}
+
+void push(int n){
+    struct node* temp = (struct node*)malloc(sizeof(struct node));
+    temp->data = n;
+    temp->next = top;
+    top = temp;
+
+}
+
+int pop(){
+    struct node* p;
+    if (isEmpty) 
+    {
+       return -1;
+    }
+            p = top;
+            top = top->next;
+            int x = p->data;
+            free(p);
+            return x;
+            
+}
+
+int main() {
+push(20);
+push(25);
+push(10);
+push(15);
+int l = pop();
+
+printf(" Popped Number is %d",l);
+
+print();
+
+return 0;
+}
